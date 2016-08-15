1973f4413a5f3947219f8c37b6ea6bb814938271
peccu <peccul@gmail.com>
2016-08-16 03:21:29 +0900

    update questions components

--- client/app/questions/questions.component.js
+++ client/app/questions/questions.component.js
@@ -7,9 +7,24 @@ import routes from './questions.routes';
 
 export class QuestionsComponent {
   /*@ngInject*/
-  constructor() {
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('questions');
+    });
+
     this.message = 'Hello';
   }
+
+  $onInit() {
+    this.$http.get('/api/questions')
+      .then(response => {
+        this.questions = response.data;
+        this.socket.syncUpdates('questions', this.questions);
+      });
+  }
+
 }
 
 export default angular.module('qaApp.questions', [ngRoute])
