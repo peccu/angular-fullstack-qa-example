041e58530db6580bdde71a447ac2b44bf969f5f4
peccu <peccul@gmail.com>
2016-08-16 01:41:59 +0900

    initial commit with other yeoman options

---
+++ client/app/account/settings/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import SettingsController from './settings.controller';
+
+export default angular.module('qaApp.settings', [])
+  .controller('SettingsController', SettingsController)
+  .name;
